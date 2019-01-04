e406ec142ab9a7451dc8955172a2025a81b2892a
Sparsh Sharma <sparsh.sharma@mail.ru>
2019-01-04 17:44:33 +0100

    added the changes introduced by sahitya into the main files

--- vortex_location_sahitya.py
+++ vortex_location_sahitya.py
@@ -32,31 +32,51 @@ for i in range (100, 500):
 print xvort1  
 print vel1
 
-x = 15
-y = 1
-q = np.array([x,y]).T
-
 xvort2 = np.zeros((500,2))
 vel2 = np.zeros((500,2))
+q = np.zeros((500,2))
 a = 0
 
 
 for i in range (0, 500):
+    
     i = i * 0.1
+    q[a,0] = 15
+    q[a,1] = 1
     xvort2[a,0] = i
     xvort2[a,1] = 0
     vel2[:,:] = panels.induced_velocity_single(q, xvort2, -0.4)
     a = a + 1
 
 
-print xvort2  
+print xvort2 
 print vel2
+
+# x = 15
+# y = 1
+# q = np.array([x,y]).T
+
+# xvort2 = np.zeros((500,2))
+# vel2 = np.zeros((500,2))
+# a = 0
+
+
+# for i in range (0, 500):
+#     i = i * 0.1
+#     xvort2[a,0] = i
+#     xvort2[a,1] = 0
+#     vel2[:,:] = panels.induced_velocity_single(q, xvort2, -0.4)
+#     a = a + 1
+
+
+# print xvort2  
+# print vel2
 
 vel3 = np.zeros((500,2))
 vel3 = vel1 + vel2
 print vel3
 
-savetxt('single-vortex_array'+str()+'.csv',np.column_stack((vel1[:],vel2[:],vel3[:])), fmt='%5s', delimiter=',')
+savetxt('single-vortex_array_sp'+str()+'.csv',np.column_stack((vel1[:],vel2[:],vel3[:])), fmt='%5s', delimiter=',')
 
 t = np.linspace(0,50,500)
 
@@ -81,7 +101,7 @@ v1 = plt.plot(t, vel1[:,1], c='r', label='-0.24, v')
 u2 = plt.plot(t, vel2[:,0], c='k', label='-0.48, u')
 v2 = plt.plot(t, vel2[:,1], c='g', label='-0.48, v')
 plt.legend(loc='upper left')
-savefig('induced_velocity__location_array.pdf')
+savefig('induced_velocity__location_array_sp.pdf')
 plt.show()
 
 fig, ax = plt.subplots()
@@ -89,7 +109,7 @@ vx3 = plt.plot(t, vel3[:,0], c='r', label='x_tangential')
 vy3 = plt.plot(t, vel3[:,1], c='g', label='y_tangential')
 v3_mag = plt.plot(t, vel3_mag[:], c='b', label='induced_velocity')
 plt.legend(loc='best')
-savefig('induced_velocity_location_tot_array.pdf')
+savefig('induced_velocity_location_tot_array_sp.pdf')
 plt.show()
 
 
